e2a17da0cd0bffd63e4450207b82cb513eafb765
wangyan4 <2236995139@qq.com>
2020-03-16 12:52:41 +0800

    usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,52 @@
-module.exports=function(grunt){
+module.exports = function (grunt) {
   grunt.initConfig({
-    clean:{
-      src:"dist/"
+    clean: {
+      src: "dist/"
     },
     htmlmin: {
       options: {
         collapseWhitespace: true,
-        preserveLineBreaks: false
+        removeComments: true
       },
       files: {
+        src: './dist/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['./dist/index.html']
+    },
+    uglify: {
+      'dist/index.min.js': ['index.js']
+    },
+    cssmin: {
+      'dist/index.min.css': ['index.css']
+    },
+    copy: {
+      html: {
         src: './index.html',
         dest: 'dist/index.html'
       }
     },
-    cssmin: {
-      "dist/index.css":"index.css"
-    },
-    uglify: {
-      "dist/index.js":"index.js"
-    },
-    imagemin: {                               
-      files: {
-        expand: true,
-        src: ['images/*.{png,jpg,gif}'],
-        dest: 'dist/images'
+    imagemin: {
+      dist: {
+          options: {
+              optimizationLevel: 1
+          },
+          files: [{
+              expand: true,
+              cwd: 'images/',
+              src: ['**/*.{png,jpg,jpeg,gif}'],
+              dest: 'dist/images/'
+          }]
       }
-    },
-    copy: {
-      html: {
-          src:'./index.html',
-          dest:'./dist/index.html'
-      }
-    },
-    useminPrepare: {
-      html:'index.html',
-      options:{
-          dest:'dist'
-      }
-    },
-    usemin: {
-      html:['dist/index.html']
-    }
+  },
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
@@ -48,7 +54,9 @@ module.exports=function(grunt){
   grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-usemin');
 
-  grunt.registerTask('default',['clean','copy:html', 'useminPrepare', 'uglify', 'usemin', 'cssmin', 'htmlmin']);
+  // grunt.registerTask('default', ['clean', 'copy','useminPrepare', 'uglify','usemin', 'cssmin', 'htmlmin','imagemin']);
+  grunt.registerTask('default',['imagemin']);
 }
